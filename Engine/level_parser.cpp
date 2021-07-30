b6304560b58e27d753a6d12b9cec9864f11c645e
Infra-Blue <rudro1152@gmail.com>
2021-07-30 22:39:41 +0600

    Isometric Projection generated. Good debug session

--- Engine/level_parser.cpp
+++ Engine/level_parser.cpp
@@ -7,22 +7,33 @@
 #include "../Assets/tile.h"
 
 
-/***************************************************
- * MAYBE USE AN ENUM CLASS TO SORT OUT GLOBALUNIT ? *
- *   MAYBE SET CUSTOM DESTINATION FOR EACH BOT ?   *
- ***************************************************/
+// Functions for IsoMetric Calculations
+void isometricIncrementX (sf::Vector2f& vec, int step)
+{
+    vec.x += step * tile::unit;
+    vec.y += step * 0.5f * tile::unit;
+}
 
+void isometricDecrementX (sf::Vector2f& vec, int step)
+{
+    vec.x -= step * tile::unit;
+    vec.y -= step * 0.5f * tile::unit;
+}
 
-/**************************
- *   * FOR BLANK TILE   *
- *    - FOR VOID TILE    *
- *    [0 ~ 9] FOR DATA    *
- * ! FOR DESTINATION TILE *
- **************************/
+void isometricIncrementY (sf::Vector2f& vec, int step)
+{
+    vec.x -= step * tile::unit;
+    vec.y += step * 0.5f * tile::unit;
+}
+
+void isometricDecrementY (sf::Vector2f& vec, int step)
+{
+    vec.x += step * tile::unit;
+    vec.y -= step * 0.5f * tile::unit;
+}
 
 
 // Mapped tile data to a specific pair of colors
-// The ONLY Golabl varibale of the Project
 std::map<char, std::pair<sf::Color, sf::Color>> palette = {
     {'*', std::make_pair(sf::Color(0xbdbdbdff), sf::Color(0x8d8d8dff))},
     {'0', std::make_pair(sf::Color(0xec407aff), sf::Color(0xb4004eff))},
@@ -53,8 +64,8 @@ void loadLevel (int levelNumber, std::vector<std::vector<tile*>>& tileMatrix)
 
     int rowCount = 0, colCount = 0;
     std::vector<std:: string> dataMatrix;  // Matrix for storing data written on tiles
-
     
+
     // Extracting the tile data matrix from Level file
     while (std::getline(levelFile, rowBuffer)) {
         if (rowBuffer == "")
@@ -63,18 +74,40 @@ void loadLevel (int levelNumber, std::vector<std::vector<tile*>>& tileMatrix)
         dataMatrix.push_back(rowBuffer);
     }
     
+
     rowCount = dataMatrix.size();
     colCount = dataMatrix[0].size();
-    
 
+    // Resizing the empty tileMatrix to fit all tiles
+    for (int i = 0; i < rowCount; ++i) {
+        tileMatrix.push_back(std::vector<tile*>());
+        tileMatrix[i].resize(colCount);
+    }
+    
+    
     // Extracting the writability matrix; and then initializing the tile matrix; since now we have everything we need
+    int y = 0;
+    sf::Vector2f currentPosition(colCount * tile::unit, 0.5f * tile::unit);
+
     while (std::getline(levelFile, rowBuffer)) {
         if (rowBuffer == "")
             break;
 
+        for (int x = 0; x < colCount;) {
+            if (rowBuffer[x] == '-')
+                tileMatrix[y][x] = new void_tile();
+            else if (rowBuffer[x] == '0')
+                tileMatrix[y][x] = new non_initializable_tile(dataMatrix[y][x], currentPosition);
+            else
+                tileMatrix[y][x] = new initializable_tile(dataMatrix[y][x], currentPosition);
 
+            ++x;
+            isometricIncrementX(currentPosition, 1);
+        }
+        isometricDecrementX(currentPosition, colCount);
+        isometricIncrementY(currentPosition, 1); 
+        ++y;
     }
-    
 
     levelFile.close();
 }
